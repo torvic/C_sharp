b7be33d4991d16e553fb5920b3c69cc07ca50295
torvic <torvic02st96@gmail.com>
2023-01-16 11:44:05 -0500

    Add if statement

---
+++ ifStatement/ifStatement/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ifStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anyValue = 6;
+            string message = "";
+
+            if (anyValue == 7) 
+            {
+                message = "OMG, it's a miracle any value is 7";
+            } else
+            {
+                message = "The value wasn't 7";
+            }
+            Console.WriteLine($"the answer is > {message}");
+        }
+    }
+}
